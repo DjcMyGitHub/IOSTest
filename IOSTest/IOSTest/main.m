79dcfa84cbb08056234ef7db815439ded6672805
aleph24984 <aleph24984@aleph24984demac-mini.local>
2018-04-13 16:40:22 +0800

    第一次从xcode提交项目

---
+++ IOSTest/IOSTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  IOSTest
+//
+//  Created by aleph24984 on 18/4/13.
+//  Copyright © 2018年 aleph24984. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
